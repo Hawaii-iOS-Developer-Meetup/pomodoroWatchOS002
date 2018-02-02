18fb3f9dd79f918ba61337437d8b61f8a95d177e
davidkneely <davidics613@gmail.com>
2018-02-01 18:42:36 -1000

    Initial Commit

---
+++ pomodoroWatchOS002Tests/pomodoroWatchOS002Tests.swift
@@ -0,0 +1,36 @@
+//
+//  pomodoroWatchOS002Tests.swift
+//  pomodoroWatchOS002Tests
+//
+//  Created by David on 2/1/18.
+//  Copyright © 2018 Vision Runner. All rights reserved.
+//
+
+import XCTest
+@testable import pomodoroWatchOS002
+
+class pomodoroWatchOS002Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
